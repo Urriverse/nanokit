12e3114137ecb0ad4a474e89a37c10f41b39740f
Vi Ramen <vi.is.chapmann@gmail.com>
2026-06-27 20:28:25 +0300

    Initial commit

--- src/ph.rs
+++ src/ph.rs
@@ -1,1 +1,1 @@
-#[cfg(not(test))] #[panic_handler] pub fn phdl(pi: &core::panic::PanicInfo) -> ! { crate::KeInvoke!(panic: pi) }
+pub macro panic_handler() { #[panic_handler] pub fn phdl(pi: &core::panic::PanicInfo) -> ! { crate::KeInvoke!(panic: pi) } }
