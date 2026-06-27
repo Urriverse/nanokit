c39b59e634a5db42c7345868fc937bff3e7e8404
Vi Ramen <vi.is.chapmann@gmail.com>
2026-06-27 22:13:26 +0300

    Initial commit

--- src/log.rs
+++ src/log.rs
@@ -34,25 +34,25 @@ impl AttLvl {
 }
 
 pub macro trace($($arg:tt)+) {
-    crate::sym::k_log(AttLvl::Trace, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), &format_args!($($arg)+));
+    crate::sym::k_log(AttLvl::Trace, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), format_args!($($arg)+));
 }
 
 pub macro debug($($arg:tt)+) {
-    crate::sym::k_log(AttLvl::Trace, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), &format_args!($($arg)+));
+    crate::sym::k_log(AttLvl::Trace, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), format_args!($($arg)+));
 }
 
 pub macro info($($arg:tt)+) {
-    crate::sym::k_log(AttLvl::Trace, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), &format_args!($($arg)+));
+    crate::sym::k_log(AttLvl::Trace, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), format_args!($($arg)+));
 }
 
 pub macro warn($($arg:tt)+) {
-    crate::sym::k_log(AttLvl::Trace, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), &format_args!($($arg)+));
+    crate::sym::k_log(AttLvl::Trace, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), format_args!($($arg)+));
 }
 
 pub macro error($($arg:tt)+) {
-    crate::sym::k_log(AttLvl::Trace, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), &format_args!($($arg)+));
+    crate::sym::k_log(AttLvl::Trace, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), format_args!($($arg)+));
 }
 
 pub macro panic_msg($($arg:tt)+) {
-    crate::sym::k_log(AttLvl::Trace, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), &format_args!($($arg)+));
+    crate::sym::k_log(AttLvl::Trace, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), format_args!($($arg)+));
 }
