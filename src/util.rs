45fa5a7027564be848618a1188e24997e4683d7c
Vi Ramen <vi.is.chapmann@gmail.com>
2026-07-01 01:57:12 +0300

    Initial commit

--- src/util.rs
+++ src/util.rs
@@ -1,34 +1,11 @@
-pub macro SYMBOL (
-    $(
-        $v:vis $n:ident: $t:ty = $d:expr;
-    )+
-) {
-    $(
-        #[used]
-        #[unsafe(no_mangle)]
-        $v static $n: $t = $d;
-    )*
-}
+pub macro SYMBOL ( $( $v:vis $n:ident: $t:ty = $d:expr; )+ )
+{ $( #[used] #[unsafe(no_mangle)] $v static $n: $t = $d; )* }
 
-pub macro meta($n:expr) {
+pub macro entry ( mod $n:literal; $($b:tt)* ) {
     #[cfg(not(test))] panic_handler![];
-
     SYMBOL! { pub MODNAME: &'static str = $n; }
-}
-
-pub macro entry( $($b:tt)* ) {
     #[unsafe(no_mangle)]
     pub extern "C" fn _start() {
-        $(
-            $b
-        )*
-    }
-}
-
-pub macro Ku( $( $x:ident or $d:expr )+ ) {
-    SYMBOL! {
-        $(
-            pub $x: $x = $d;
-        )+
+        $( $b )*
     }
 }
